fb64e248f9fbf1198f43cfb3715a1fa47e189903
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-10 16:20:11 -0400

    added react-testing-library

--- src/components/Header/header.test.js
+++ src/components/Header/header.test.js
@@ -1,13 +1,11 @@
 import React from "react"
-import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
 
 import Header from "./header"
 
 describe("Header", () => {
   it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header siteTitle="Default Starter" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const { container } = render(<Header siteTitle="Default Starter" />)
+    expect(container).toMatchSnapshot()
   })
 })
