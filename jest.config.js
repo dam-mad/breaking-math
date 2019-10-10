35d1f405dead3cc351378894dbf22af27070940f
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-10 17:25:54 -0400

    fixed default props for SEO component

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': '<rootDir>/testing/config/jest-preprocess.js',
+    '^.+\\.jsx?$': '<rootDir>/testing/config/jest-preprocess.js'
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/testing/__mocks__/file-mock.js',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/testing/__mocks__/file-mock.js'
   },
   testPathIgnorePatterns: ['node_modules', '.cache', 'public'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
   globals: {
-    __PATH_PREFIX__: '',
+    __PATH_PREFIX__: ''
   },
   collectCoverage: false,
   collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
@@ -18,9 +19,9 @@ module.exports = {
     global: {
       statements: 0,
       branches: 0,
-      functions: 0,
-    },
+      functions: 0
+    }
   },
   testURL: 'http://localhost',
-  setupFiles: ['<rootDir>/testing/config/loadershim.js'],
+  setupFiles: ['<rootDir>/testing/config/loadershim.js']
 };
--- testing/__mocks__/gatsby.js
+++ testing/__mocks__/gatsby.js
@@ -7,13 +7,12 @@ module.exports = {
   graphql: jest.fn(),
   Link: jest.fn().mockImplementation(
     // these props are invalid for an `a` tag
-    ({
-      activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest
-    }) => React.createElement('a', {
-      ...rest,
-      href: to,
-    }),
+    ({ activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest }) =>
+      React.createElement('a', {
+        ...rest,
+        href: to
+      })
   ),
   StaticQuery: jest.fn(),
-  useStaticQuery: jest.fn(),
+  useStaticQuery: jest.fn()
 };
