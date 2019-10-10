ec17f1d8204141ed36eef5d87dbfe0389b20a622
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-10 16:50:49 -0400

    add eslint fix

--- testing/__mocks__/gatsby.js
+++ testing/__mocks__/gatsby.js
@@ -1,4 +1,5 @@
 const React = require('react');
+
 const gatsby = jest.requireActual('gatsby');
 
 module.exports = {
@@ -6,12 +7,13 @@ module.exports = {
   graphql: jest.fn(),
   Link: jest.fn().mockImplementation(
     // these props are invalid for an `a` tag
-    ({ activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest }) =>
-      React.createElement('a', {
-        ...rest,
-        href: to
-      })
+    ({
+      activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest
+    }) => React.createElement('a', {
+      ...rest,
+      href: to,
+    }),
   ),
   StaticQuery: jest.fn(),
-  useStaticQuery: jest.fn()
+  useStaticQuery: jest.fn(),
 };
