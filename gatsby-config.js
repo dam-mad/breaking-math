2739f1d603de710f9e40e935ce6397f218fb1bd6
Dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2019-10-10 14:52:31 -0400

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Breaking Math Podcast`,
+    description: `Official Breaking Math Podcast site.`,
+    author: `@kdabug & @dallin_r`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
